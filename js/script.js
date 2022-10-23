d83c2b6de0a1d334d5d7534be53bdbdbd699ae6d
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-23 14:34:31 -0400

    Addec only nessesitys

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ashley Monaghan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
